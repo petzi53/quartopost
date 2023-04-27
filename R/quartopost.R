7d7729120988940b9b91e69b7d0bd25c40ee93bb
petzi53 <petzi53@gmail.com>
2023-04-27 23:23:24 +0200

    replace try with stopifnot & reformat script

--- R/quartopost.R
+++ R/quartopost.R
@@ -21,20 +21,14 @@ long_yaml_text <- function(txt) {
 qp <-  function() {
     params <- get_args()
 
-    try(if (params$title == "") {
-        stop("A blog post must have a title")
-    } else {
-
+    stopifnot("Your blog post has no title!" =
+              params$title != "")
     slug <- paste0("posts/", params$date, "-",
-                        title_kebab(params$title)
-                )
-
+            title_kebab(params$title) )
     new_post_file <- paste0(slug, '/', "index.qmd")
 
-    try(if (file.exists(new_post_file)) {
-        stop("A blog post with the same file name already exists")
-    } else {
-
+    stopifnot("File name already exists!" =
+         !file.exists(new_post_file))
 
     if (params$description != "") {
         description <-
@@ -87,8 +81,6 @@ qp <-  function() {
 
     rstudioapi::documentOpen(new_post_file, line = (length(post_yaml) + 1))
     invisible()
-    }) # end of try file exists
-    }) # end of try empty title
 }
 
 
@@ -98,120 +90,94 @@ get_args <- function() {
     ui <- miniUI::miniPage(
         miniUI::miniTabstripPanel(
             miniUI::miniTabPanel("Essentials", icon = shiny::icon("sliders"),
-                                 miniUI::miniContentPanel(
-                                     shiny::fillRow(flex = c(7,4,3),
-                                                    shiny::textInput(
-                                                        inputId = "title",
-                                                        label = "Title (try to be under 40 characters)",
-                                                        placeholder = "Name of your blog post",
-                                                    ),
-                                                    shiny::textInput(
-                                                        inputId = "author",
-                                                        label = "Author",
-                                                        value = getOption("quartopost.author"),
-                                                        placeholder = "Name of your blog post",
-                                                    ),
-                                                    shiny::fillRow(shiny::dateInput(
-                                                        inputId = "date",
-                                                        label = "Date",
-                                                        value = lubridate::today(),
-                                                    ),
-                                                    ),
-                                                    height = "70px"
-                                     ),
-                                     shiny::fillRow(shiny::textInput(
-                                         inputId = "subtitle",
-                                         label = "Subtitle",
-                                         placeholder = "subtitle (optional)",
-                                         width = "100%"
-                                     ),
-                                     height = "70px"
-                                     ),
-
-                                     # shiny::fillRow(shiny::checkboxGroupInput(
-                                     #     inputId = "misc",
-                                     #     label = "Add following YAML fields:",
-                                     #     choices = c("date-modified",
-                                     #                 "draft"),
-                                     #     inline = TRUE
-                                     # ),
-                                     # height = "70px"
-                                     # ),
-
-                                 ),
+                 miniUI::miniContentPanel(
+                     shiny::fillRow(flex = c(7,4,3),
+                        shiny::textInput(
+                            inputId = "title",
+                            label = "Title (try to be under 40 characters)",
+                            placeholder = "Name of your blog post",
+                        ),
+                        shiny::textInput(
+                            inputId = "author",
+                            label = "Author",
+                            value = getOption("quartopost.author"),
+                            placeholder = "Name of your blog post",
+                        ),
+                        shiny::fillRow(shiny::dateInput(
+                            inputId = "date",
+                            label = "Date",
+                            value = lubridate::today(),
+                        ),
+                        ),
+                        height = "70px"
+                     ),
+                     shiny::fillRow(shiny::textInput(
+                         inputId = "subtitle",
+                         label = "Subtitle",
+                         placeholder = "subtitle (optional)",
+                         width = "100%"
+                     ),
+                     height = "70px"
+                     ),
+                 ),
             ),
             miniUI::miniTabPanel("Categories", icon = shiny::icon("area-chart"),
-                                 miniUI::miniContentPanel(
-                                     shiny::fillRow(shiny::selectInput(
-                                         inputId = "categories",
-                                         label = "Categories",
-                                         choices = c("Item1", "Item2", "Item3", "another Item"),
-                                         multiple = TRUE,
-                                         width = "100%"
-                                        ),
-                                    ),
-                                ),
+                 miniUI::miniContentPanel(
+                     shiny::fillRow(shiny::selectInput(
+                         inputId = "categories",
+                         label = "Categories",
+                         choices = c("Item1", "Item2", "Item3", "another Item"),
+                         multiple = TRUE,
+                         width = "100%"
+                        ),
+                    ),
+                ),
             ),
-
             # shiny::fillRow(htmltools::hr(), height = '50px'),
             miniUI::miniTabPanel("Image", icon = shiny::icon("area-chart"),
-                                 miniUI::miniContentPanel(
-                                     shiny::fillRow(
-                                         shiny::fileInput('newimg', 'Image', placeholder = 'Select external image',
-                                                          accept="image/*"),
-                                         shiny::column(width = 6, offset = 2, shiny::uiOutput('overbutton')),
-                                         height = '70px'
-                                     ),
-                                     # shiny::fillRow(
-                                     #     shiny::textInput('w', 'Width', '', "50%", '(optional) e.g., 400px or"80%'),
-                                     #     shiny::textInput('h', 'Height', '', "50%", '(optional) e.g., 200px'),
-                                     #     height = '70px'
-                                     # ),
-                                     shiny::fillRow(
-                                         shiny::textInput('alt', 'Alternative text', '', "100%", 'Replacement text when image is not available'),
-                                         height = '70px'
-                                     ),
-                                     # shiny::fillRow(
-                                     #     shiny::textInput('target', 'Target file path', '', "100%", '(optional) customize if necessary'),
-                                     #     height = '70px'
-                                     # ),
-                                 ),
+                 miniUI::miniContentPanel(
+                     shiny::fillRow(
+                         shiny::fileInput('newimg', 'Image', placeholder =
+                              'Select external image', accept="image/*"),
+                         shiny::column(width = 6, offset = 2, shiny::uiOutput('overbutton')),
+                         height = '70px'
+                     ),
+                     shiny::fillRow(
+                         shiny::textInput('alt', 'Alternative text', '', "100%",
+                              'Replacement text when image is not available'),
+                         height = '70px'
+                     ),
+                 ),
             ),
 
 
             miniUI::miniTabPanel("Description", icon = shiny::icon("table"),
-                                 miniUI::miniContentPanel(
-                                     shiny::fillRow(shiny::textAreaInput(
-                                         inputId = "description",
-                                         label = "Description",
-                                         placeholder = "Write (optional) a short description, summary or introductory paragraph. Markdown is allowed.",
-                                         width = "100%",
-                                         rows = 8
-                                     ),
-                                     height = "70px"
-                                     )
-                                 )
+                 miniUI::miniContentPanel(
+                     shiny::fillRow(shiny::textAreaInput(
+                         inputId = "description",
+                         label = "Description",
+                         placeholder =
+"Write (optional) a short description, summary or introductory paragraph. Markdown is allowed.",
+                         width = "100%",
+                         rows = 8
+                     ),
+                     height = "70px"
+                     )
+                 )
             )
         ),
         miniUI::gadgetTitleBar("Enter the YAML fields for your post")
     )
 
     server <- function(input, output, session) {
-        # Define reactive expressions, outputs, etc.
-        output$title <- shiny::renderText({ input$title })
-        output$author <- shiny::renderText({ input$author })
-        # my_list = paste(shiny::renderText( { input$title } ),
-        #                 shiny::renderText( { input$author })
-        #                 )
 
         # When the Done button is clicked, return a value
         shiny::observeEvent(input$done, {
-            returnValue <- list(input$title, input$author, input$date,
-                                input$newimg, input$alt,
-                                input$subtitle, input$description)
+            returnValue <- list(input$title, input$author,
+                input$date, input$newimg, input$alt,
+                input$subtitle, input$description)
             names(returnValue) <- c("title", "author", "date",
-                                    "image", "alt",
-                                    "subtitle", "description")
+                "image", "alt", "subtitle", "description")
             shiny::stopApp(returnValue)
         })
     }
