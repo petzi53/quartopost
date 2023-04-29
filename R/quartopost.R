5a9496cf8d93d7d8f8adc3cdcc61cc66f5c1ca00
petzi53 <petzi53@gmail.com>
2023-04-29 19:54:17 +0200

    export and document quartopost()

--- R/quartopost.R
+++ R/quartopost.R
@@ -1,3 +1,18 @@
+#' Create a new post
+#'
+#' `quartopost()` opens a dialog window to input title and other
+#' desired data to create the file for the blog post.
+#'
+#' @return Nothing. What matters are the side effects:
+#' - create a folder in kebab notation with the name of date followed by the post title
+#' - create a "index.qmd" file
+#' - copy the (optional) image file into the folder
+#' - populate the YAML field
+#' - open the file for editing
+#' @export
+#'
+#' @examples
+#' quartopost()
 quartopost <-  function() {
 
     params <- get_args()
